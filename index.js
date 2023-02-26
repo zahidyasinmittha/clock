f366728f59dbfb0a1fb8262f1495043ebf6ac196
zahid yasin <b.zahidmittha@gmail.com>
2023-02-26 17:55:31 +0500

    V3
    am and pm issue resolved

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function time(){
     hour_12=hour_12-12;
     digital_time=`0${hour_12}:`;
     digital_time+=(minute>9)?`${minute}:`:`0${minute}:`;
-    digital_time+=(second>9) ? `${second} AM` : `0${second} AM`;
+    digital_time+=(second>9) ? `${second} PM` : `0${second} PM`;
     digital_clock.innerHTML=digital_time;
     }
     else{
         digital_time=(hour_12>9)?`${hour_12}:`:`0${hour_12}:`;
         digital_time+=(minute>9)?`${minute}:`:`0${minute}:`;
-        digital_time+=(second>9) ? `${second} PM` : `0${second} PM`;
+        digital_time+=(second>9) ? `${second} AM` : `0${second} AM`;
         digital_clock.innerHTML=digital_time;
     }
 
